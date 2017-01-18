fce5c05b846c805f7d8e24885ddcedeb8d50cefa
trandushev <trand@abv.bg>
2017-01-19 00:06:31 +0200

    Initial commit

---
+++ Udacity.js
@@ -0,0 +1,4 @@
+
+/**
+ * Created by trand on 17.1.2017 г..
+ */
---
+++ js/resumeBuilder.js
@@ -0,0 +1,11 @@
+//var formattedName = HTMLheaderName.replace("%data%", 'Nikolay');
+//var formattedRole = HTMLheaderRole.replace("%data%", 'Web developer');
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+
+
+var cars = ["Nikolaj", "KOko"];
+
+//$("#main").append(name);
+$("#main").append(cars);
+$("#main").append(cars.length);
